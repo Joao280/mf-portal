13685fdbc551525f25c31890733eae33f3aa39e5
Joao280 <joaoc2800@gmail.com>
2022-03-15 14:28:52 +0000

    format code

--- src/joaoc-root-config.ts
+++ src/joaoc-root-config.ts
@@ -7,27 +7,27 @@ import {
 import { RoutesConfig } from "single-spa-layout/dist/types/isomorphic/constructRoutes";
 
 function getTemplate() {
-  fetch('http://localhost:3000/template')
-    .then(response => {
+  fetch("http://localhost:3000/template")
+    .then((response) => {
       if (!response.ok) {
-          throw new Error(response.statusText)
-        }
-        return response.json() as Promise<RoutesConfig>
+        throw new Error(response.statusText);
+      }
+      return response.json() as Promise<RoutesConfig>;
     })
-    .then(template => {
-          const routes = constructRoutes(template)
-          const applications = constructApplications({
-      routes,
-      loadApp({ name }) {
-        return System.import(name);
-      },
-    });
-    const layoutEngine = constructLayoutEngine({ routes, applications });
+    .then((template) => {
+      const routes = constructRoutes(template);
+      const applications = constructApplications({
+        routes,
+        loadApp({ name }) {
+          return System.import(name);
+        },
+      });
+      const layoutEngine = constructLayoutEngine({ routes, applications });
 
-    applications.forEach(registerApplication);
-    layoutEngine.activate();
-    start();
-    })
+      applications.forEach(registerApplication);
+      layoutEngine.activate();
+      start();
+    });
 }
 
 getTemplate();
